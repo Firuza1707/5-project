95a0d34b9f0bf54409a5cd4e2d010be6fc33c50f
Firuza1707 <niyazovafiruza1707@gmail.com>
2024-07-01 16:50:26 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,13 @@ function App() {
 
       {/* section */}
       <section>
-        <div className="container mt-3">
+        <div className="container d-flex">
           <div className="row d-flex ">
             <div className="col-lg-8">
 
-              <div className="card mb-2 d-flex ">
+              <div className="card mb-4 d-flex ">
                 <a href="#">
-                  <img src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="" />
+                  <img src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="" className='img-fluid' />
                 </a>
                 <div className="card-body">
                   <div className="small text-muted">January 1,2023</div>
@@ -44,6 +44,9 @@ function App() {
                   <a href="#" className="btn btn-primary">Read more</a>
                 </div>
               </div>
+
+
+
 
               <div className="row  d-flex">
                 <div className="col-lg-6">
@@ -62,7 +65,6 @@ function App() {
                     </div>
                   </div>
 
-
                   <div className='card mb-4 d-flex '>
                     <a href="#">
                       <img src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" className='img-fluid' />
@@ -76,6 +78,7 @@ function App() {
                       <a href="#" className='btn btn-primary'>Read more</a>
                     </div>
                   </div>
+
                 </div>
 
 
@@ -115,8 +118,96 @@ function App() {
 
             </div>
           </div>
+
+          
+
+          <div className="col-lg-4 ">
+            <div className="card mb-4 d-flex">
+              <div className="card-header">Search</div>
+              <div className="card-body">
+                <div className="input-group ">
+                  <input type="text" className="form-control " placeholder='Enter search terms...' aria-label='Enter search terms...' aria-describedby='button-search' />
+                  <button className="btn btn-primary" id='button-search' type='button'>Go!</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="card mb-4 d-flex">
+              <div className="card-header">Categories</div>
+              <div className="card-body">
+                <div className="row d-flex ">
+                  <div className="col-sm-6">
+                    <ul className="list-unstyled mb-0">
+                      <li>
+                        <a href="#">Web Design</a>
+                      </li>
+                      <li>
+                        <a href="#">HTML</a>
+                      </li>
+                      <li>
+                        <a href="#">Freebies</a>
+                      </li>
+                    </ul>
+                  </div>
+
+
+                  <div className="col-sm-6">
+                    <ul className="list-unstyled mb-0">
+                      <li>
+                        <a href="#">JavaScript</a>
+                      </li>
+                      <li>
+                        <a href="#">CSS</a>
+                      </li>
+                      <li>
+                        <a href="#">Tutorials</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="card mb-4 d-flex">
+              <div className="card-header">Side Widget</div>
+              <div className="card-body">
+                "You can put anyting you want inside of thses side widgets. They are easy to use, and features the Bootstrap 5 card component!"
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </section>
+
+      <nav aria-label='Pagination'>
+            <hr className="my-0" />
+            <ul className='pagination justify-content-center my-4 d-flex'>
+              <li className="page-item disabled">
+                <a href="#" className='page-link' tabIndex={1} aria-disabled="true">Newer</a>
+              </li>
+              <li className='page-item active' aria-current="page">
+                <a href="#" className='page-link'>1</a>
+              </li>
+              <li className='page-item ' >
+                <a href="#" className='page-link'>2</a>
+              </li>
+              <li className='page-item ' >
+                <a href="#" className='page-link'>3</a>
+              </li>
+              <li className='page-item disabled' >
+                <a href="#" className='page-link'>...</a>
+              </li>
+              <li className='page-item ' >
+                <a href="#" className='page-link'>15</a>
+              </li>
+              <li className='page-item ' >
+                <a href="#" className='page-link'>Older</a>
+              </li>
+            </ul>
+          </nav>
+
+
       <footer className='py-5 bg-dark'>
         <div className="container">
           <p className='m-0 text-center text-white'>Copyright © Your Website 2023</p>
